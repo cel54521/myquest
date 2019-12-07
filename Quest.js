2506409bdcf5205dc49bf946060aec355b00c51d
cel54521 <cel54521>
2019-12-08 00:21:39 +0900

    変更

--- Quest.js
+++ Quest.js
@@ -2,19 +2,7 @@
 
 // タスクオブジェクト
 var Quest = function(questName, date, priority){
-    var _questName = questName;
-    var _date = date;
-    var _priority = priority;
-
-    Object.defineProperties(this, {
-        // タスク
-        questName: {
-            get: function() {
-                return _questName;
-            },
-            set: function(questName) {
-                _questName = questName;
-            }
-        }
-    });
+    this.questName = questName;
+    this.date = date;
+    this.priority = priority;
 };
